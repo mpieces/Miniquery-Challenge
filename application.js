e9a6082147df1f21b709fc11968dfb4480681665
Andrew Pham & Marisa Ramon <pair+atp1o2.mpieces@devbootcamp.com>
2015-08-21 14:55:01 -0700

    create miniquery library that selects by id, class, tag name, hides & shows elements, adds & removes classes; end of release 1

---
+++ application.js
@@ -0,0 +1,20 @@
+  // select: function(selector) {
+  //   var type = selector[0]
+  //   if(type === "#"){ return document.getElementById(selector.substring(1)) }
+  // }
+
+  // return {
+  //   select: select
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
